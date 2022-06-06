b32b9ad710e89b935c7928196d69961c76182f14
Luke <lukedwoods@gmail.com>
2022-06-06 15:20:30 +0000

    Add user.csv dataframe to showcase new df display

--- pages/luke_page.py
+++ pages/luke_page.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas as pd
+
+# st.set_page_config(page_title="Luke's Page", page_icon="🌍")
 
 st.title("Luke's Page")
+
+df = pd.read_csv('tmp/users.csv')
+st.dataframe(df)
